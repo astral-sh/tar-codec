b2db9e388352dab1f9b08991feeae0ec41ef233e
William Woodruff <william@yossarian.net>
2026-06-09 12:31:01 -0700

    More doc
    Signed-off-by: William Woodruff <william@yossarian.net>

--- crates/tar-codec/src/lib.rs
+++ crates/tar-codec/src/lib.rs
@@ -1,6 +1,15 @@
 //! High-level decoding and encoding for tar archives.
 //!
 //! See [`decode`] for decoding/extraction and [`encode`] for pax encoding.
+//!
+//! ## Security
+//!
+//! Like other tar parsers, tar-codec assumes that it has unique access
+//! to the target directory (the "extraction root") when extracting.
+//! Concurrent mutation of the target directory is outside of the threat
+//! model.
+//! See the [repository's SECURITY.md](https://github.com/astral-sh/tar-codec/blob/main/SECURITY.md)
+//! for more information.
 
 mod blocking;
 mod name;
